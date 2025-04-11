c98a25fc721fae4b4625e9037415ec2d3e550402
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-04-11 14:17:29 +0530

    Took Prompt for Players Names

--- Dice Challenge/index.js
+++ Dice Challenge/index.js
@@ -1,6 +1,10 @@
 let count = 0,
   player1Score = 0,
   player2Score = 0;
+let p1Name = "Player-1";
+let p2Name = "Player-2";
+
+takeUserDet();
 document
   .querySelector("#roll-btn")
   .addEventListener("click", handleRollButtonClick);
@@ -13,10 +17,25 @@ function handleReplayClick() {
   player1Score = 0;
   player2Score = 0;
   count = 0;
-  document.querySelector("#p1").textContent = "Player-1";
-  document.querySelector("#p2").textContent = "Player-2 ";
+  document.querySelector("#p1").textContent = p1Name;
+  document.querySelector("#p2").textContent = p2Name;
   document.querySelector("h1").innerHTML = "5-Roll Dice Battle";
   document.querySelector("#turn").innerHTML = "You have 5 turns left";
+  takeUserDet();
+}
+function takeUserDet() {
+  p1Name = prompt("Please enter Player-1's Name:");
+
+  p2Name = prompt("Please enter Player-2's Name:");
+
+  if (p1Name.trim().length === 0) {
+    p1Name = "Player-1";
+  }
+  if (p2Name.trim().length === 0) {
+    p2Name = "Player-2";
+  }
+  document.querySelector("#p1").textContent = p1Name;
+  document.querySelector("#p2").textContent = p2Name;
 }
 function handleRollButtonClick() {
   let randomInt = Math.floor(Math.random() * 6 + 1);
@@ -33,17 +52,20 @@ function handleRollButtonClick() {
   console.log("The Button is clicked", count, "number of times");
   document.querySelector("#turn").innerHTML =
     "You have " + (5 - count) + " turns left";
-  document.querySelector("#p1").textContent = "Player-1 score: " + player1Score;
-  document.querySelector("#p2").textContent = "Player-2 score: " + player2Score;
+  console.log(p1Name + " score: " + player1Score);
+  document.querySelector("#p1").textContent =
+    p1Name + " score: " + player1Score;
+  document.querySelector("#p2").textContent =
+    p2Name + " score: " + player2Score;
 
   if (count == 5) {
     document.querySelector("#turn").innerHTML = "Game is over";
     if (player1Score > player2Score) {
-      document.querySelector("h1").innerHTML = "Player-1 won";
+      document.querySelector("h1").innerHTML = p1Name + " won";
     } else if (player1Score == player2Score) {
       document.querySelector("h1").innerHTML = "It's a Draw";
     } else {
-      document.querySelector("h1").innerHTML = "Player-2 won";
+      document.querySelector("h1").innerHTML = p2Name + " won";
     }
     document.querySelector("#roll-btn").classList.toggle("hide-element");
     document.querySelector("#reply-btn").classList.toggle("hide-element");
